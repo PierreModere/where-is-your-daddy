4ba4640b5c00cbd603b57c74ffc6317ddf56c5e5
Pierre Modéré <pierre.martiniere72@gmail.com>
2022-12-15 23:48:33 +0100

    Add mapbox map ^_+

--- main.js
+++ main.js
@@ -1,68 +1,56 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
+import mapboxgl from "mapbox-gl"; // Récupère l'élément HTML représentant le bouton
+var button = document.getElementById("random-button");
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+// Récupère l'élément HTML qui contiendra la carte Mapbox
+var mapContainer = document.getElementById("map");
 
-document.querySelector("#app").innerHTML = `
-	<div id="container">
-	<img id="image" width="50%" src="https://static01.nyt.com/images/2022/10/31/well/WELL-CROWD-CRUSH1/WELL-CROWD-CRUSH1-videoSixteenByNine3000.jpg"/>
-	</div>
-	<button id="button" style="margin:auto">Where is my daddy ????</button>`;
+// Récupère l'élément HTML qui contiendra la pop-up HTML
+var popup = document.getElementById("popup");
 
-// Récupération de l'élément HTML qui contient l'image
-const imgElement = document.getElementById("image");
+// Récupère l'élément HTML représentant le bouton de fermeture de la pop-up
+var closeButton = document.getElementById("close-button");
 
-// Récupération de l'élément HTML qui contient le bouton
-const buttonElement = document.getElementById("button");
+// Initialise la carte Mapbox en utilisant l'élément HTML précédemment récupéré
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibW9kZXJlIiwiYSI6ImNsYnBtbHRuaDA5Zzgzb2xlaG5wOHhqdDgifQ._VQnRVKLWzg2H--6Md8FUQ";
 
-const container = document.getElementById("container");
+var map = new mapboxgl.Map({
+  container: mapContainer,
+  style: "mapbox://styles/mapbox/satellite-streets-v12", // Style de la carte
+  center: [-74.5, 40], // Emplacement initial de la carte
+  zoom: 1, // Niveau de zoom initial
+});
 
-// Ajout d'un gestionnaire d'événement pour l'événement "click" sur le bouton
-buttonElement.addEventListener("click", () => {
-  // Récupération de l'élément HTML qui contient le cercle rouge, s'il existe
-  const existingCircle = container.querySelector(".red-circle");
+// Ajoute un contrôle de type "bouton" à la carte
+map.addControl(
+  new mapboxgl.NavigationControl({
+    showCompass: false,
+  }),
+  "top-left"
+);
+document.querySelector(".mapboxgl-control-container").remove();
 
-  // Si le cercle rouge existe déjà, on le supprime
-  if (existingCircle) {
-    existingCircle.remove();
-  }
+button.addEventListener("click", function (e) {
+  // Génère des coordonnées géographiques au hasard
+  var lat = Math.random() * 180 - 90; // Nombre aléatoire entre -90 et 90
+  var lng = Math.random() * 360 - 180; // Nombre aléatoire entre -180 et 180
+  console.log(lat, lng);
+  // Utilise l'API de Mapbox pour ajouter un nouveau marqueur à l'emplacement spécifié
+  // par les coordonnées générées aléatoirement
+  var marker = new mapboxgl.Marker({ color: "#EE6464" })
+    .setLngLat([lng, lat])
+    .addTo(map);
 
-  // Calcul des dimensions de l'image
-  const imgRect = imgElement.getBoundingClientRect();
+  map.flyTo({
+    center: [lng, lat],
+    duration: 7000,
+    essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+    zoom: 9,
+  });
 
-  // Génération d'une position aléatoire pour le cercle rouge
-  const x = Math.random() * imgRect.width;
-  const y = Math.random() * imgRect.height;
-
-  // Création du cercle rouge
-  const circle = document.createElement("div");
-  circle.classList.add("red-circle");
-  circle.style.position = "absolute";
-  circle.style.left = `${x}px`;
-  circle.style.top = `${y}px`;
-  circle.style.width = "50px";
-  circle.style.height = "50px";
-  circle.style.borderRadius = "50%";
-  circle.style.backgroundColor = "transparent";
-  circle.style.border = "5px solid red";
-
-  // Ajout du cercle rouge à l'image
-  container.appendChild(circle);
+  // Ajoute un pop-up contenant du texte au marqueur
+  // var popup = new mapboxgl.Popup()
+  //   .setHTML("<p>Point géographique au hasard</p>")
+  //   .addTo(marker);
 });
