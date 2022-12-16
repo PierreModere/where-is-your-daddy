5fac12c10383852e6a1b82b65aa5b27feb418572
Pierre Modéré <pierre.martiniere72@gmail.com>
2022-12-16 02:51:34 +0100

    Add custom marker m( =∩王∩= )m

--- main.js
+++ main.js
@@ -1,35 +1,25 @@
 import "./style.css";
 import mapboxgl from "mapbox-gl"; // Récupère l'élément HTML représentant le bouton
-var button = document.getElementById("random-button");
 
-// Récupère l'élément HTML qui contiendra la carte Mapbox
+document
+  .getElementById("random-button")
+  .addEventListener("click", placeNewPoint);
+document.querySelector(".btn__reload").addEventListener("click", () => {
+  window.location.reload();
+});
+
 var mapContainer = document.getElementById("map");
 
-// Récupère l'élément HTML qui contiendra la pop-up HTML
-var popup = document.getElementById("popup");
-
-// Récupère l'élément HTML représentant le bouton de fermeture de la pop-up
-var closeButton = document.getElementById("close-button");
-
-// Initialise la carte Mapbox en utilisant l'élément HTML précédemment récupéré
 mapboxgl.accessToken =
   "pk.eyJ1IjoibW9kZXJlIiwiYSI6ImNsYnBtbHRuaDA5Zzgzb2xlaG5wOHhqdDgifQ._VQnRVKLWzg2H--6Md8FUQ";
 
 var map = new mapboxgl.Map({
   container: mapContainer,
-  style: "mapbox://styles/mapbox/satellite-streets-v12", // Style de la carte
-  center: [-74.5, 40], // Emplacement initial de la carte
-  zoom: 1, // Niveau de zoom initial
+  style: "mapbox://styles/mapbox/satellite-streets-v12",
+  center: [-74.5, 40],
+  zoom: 1,
 });
 
-// Ajoute un contrôle de type "bouton" à la carte
-map.addControl(
-  new mapboxgl.NavigationControl({
-    showCompass: false,
-  }),
-  "top-left"
-);
-document.querySelector(".mapboxgl-control-container").remove();
 function placeNewPoint() {
   document
     .querySelector("#titleScreen")
@@ -38,19 +28,19 @@ function placeNewPoint() {
     .querySelector("#slidingScreen")
     .classList.add("slidingScreen-transition");
   // Génère des coordonnées géographiques au hasard
-  var lat = Math.random() * (51.03457 - 41.59101) + 41.59101; // Nombre aléatoire entre 41.59101 et  51.03457
-  var lng = Math.random() * (9.45 - -4.65) + -4.65; // Nombre aléatoire entre -4.65 et  9.45
-  // Utilise l'API de Mapbox pour ajouter un nouveau marqueur à l'emplacement spécifié
-  // par les coordonnées générées aléatoirement
-  var marker = new mapboxgl.Marker({ color: "#EE6464" })
-    .setLngLat([lng, lat])
-    .addTo(map);
+  var lat = Math.random() * (51.03457 - 41.59101) + 41.59101;
+  var lng = Math.random() * (9.45 - -4.65) + -4.65;
+
+  const el = document.createElement("div");
+  el.className = "marker";
+
+  var marker = new mapboxgl.Marker(el).setLngLat([lng, lat]).addTo(map);
 
   map.flyTo({
     center: [lng, lat],
     duration: 7000,
     essential: true, // this animation is considered essential with respect to prefers-reduced-motion
-    zoom: 9,
+    zoom: 7,
   });
 
   map.on("moveend", () => {
@@ -75,6 +65,4 @@ function getLayerType(x, y) {
     });
     return displayFeat;
   });
-  console.log(displayFeatures);
 }
-button.addEventListener("click", placeNewPoint);
