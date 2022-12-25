98843150c8b1643456a104810addab07b90d9173
Pierre Modéré <pierre.martiniere72@gmail.com>
2022-12-25 04:12:24 +0100

    Add random date and place generation ~(=^‥^)ノ

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import "./style.css";
 import mapboxgl from "mapbox-gl"; // Récupère l'élément HTML représentant le bouton
+import { faker } from "@faker-js/faker";
+
+// sets locale to fr
+faker.locale = "fr";
 
 var isPopUpShowed = false;
 
@@ -46,26 +50,8 @@ function placeNewPoint() {
   });
 
   map.on("moveend", () => {
-    const markerElmt = document
-      .querySelector(".mapboxgl-marker")
-      .getBoundingClientRect();
-    getLayerType(markerElmt.top, markerElmt.right);
-  });
-}
-
-function getLayerType(x, y) {
-  const features = map.queryRenderedFeatures([x, y]);
-
-  // Limit the number of properties we're displaying for
-  // legibility and performance
-  const displayProperties = ["layer"];
-
-  const displayFeatures = features.map((feat) => {
-    const displayFeat = {};
-    displayProperties.forEach((prop) => {
-      displayFeat[prop] = feat[prop];
-    });
-    return displayFeat;
+    setRandomData();
+    showPopUp();
   });
 }
 
@@ -74,5 +60,87 @@ function showPopUp() {
     document
       .querySelector(".pop-up__container")
       .classList.add("pop-up__container--show");
+    isPopUpShowed = !isPopUpShowed;
   }
+}
+
+function addOrdinal(day) {
+  // Ajouter "st" pour les jours 1, 21 et 31
+  if (day === 1 || day === 21 || day === 31) {
+    return `${day}st`;
+  }
+  // Ajouter "nd" pour les jours 2 et 22
+  else if (day === 2 || day === 22) {
+    return `${day}nd`;
+  }
+  // Ajouter "rd" pour les jours 3 et 23
+  else if (day === 3 || day === 23) {
+    return `${day}rd`;
+  }
+  // Ajouter "th" pour tous les autres jours
+  else {
+    return `${day}th`;
+  }
+}
+
+function randomDate() {
+  // Générer des nombres aléatoires pour le jour, le mois et l'année
+  var day = Math.floor(Math.random() * 31) + 1;
+  var month = Math.floor(Math.random() * 12) + 1;
+  var year = Math.floor(Math.random() * 100) + 1950;
+
+  // Créer un tableau de mois en anglais
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  // Ajouter l'indicateur ordinal en anglais au jour
+  day = addOrdinal(day);
+
+  // Retourner la date sous le format demandé
+  return `${day} ${months[month - 1]} ${year}`;
+}
+
+function randomPlace() {
+  // Créer un tableau de types d'établissements
+  var establishmentTypes = [
+    "bar",
+    "restaurant",
+    "boite de nuit",
+    "parc d'attraction",
+  ];
+
+  // Générer un nombre aléatoire pour sélectionner un type d'établissement
+  var establishmentType =
+    establishmentTypes[Math.floor(Math.random() * establishmentTypes.length)];
+
+  // Générer un nom aléatoire en utilisant l'API de Faker.js en fonction du type d'établissement
+  var establishmentName;
+  if (establishmentType === "bar") {
+    establishmentName = faker.company.companyName() + " Bar";
+  } else if (establishmentType === "restaurant") {
+    establishmentName = faker.company.companyName() + " Restaurant";
+  } else if (establishmentType === "boite de nuit") {
+    establishmentName = faker.company.companyName() + " Boîte de nuit";
+  } else if (establishmentType === "parc d'attraction") {
+    establishmentName = faker.company.companyName() + " Parc d'attraction";
+  }
+  const adress = faker.address.zipCode() + ", " + faker.address.city();
+  return establishmentName + " - " + adress;
+}
+
+function setRandomData() {
+  document.querySelector(".pop-up__adress").textContent = randomPlace();
+  document.querySelector(".pop-up__date").textContent = randomDate();
 }
